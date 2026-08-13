5218593ba4f1971f969ebf5359400a490b59af53
Tanmesh <tanmeshofficial@gmail.com>
2026-08-13 22:54:57 +0530

    fix: resolve syntax error in FreeResources.jsx to unblock Vercel production build

--- frontend/src/pages/FreeResources.jsx
+++ frontend/src/pages/FreeResources.jsx
@@ -312,7 +312,7 @@ export default function FreeResources() {
                     </div>
                   </div>
                 </div>
-              ))}
+              )))}
             </div>
 
             {/* Right gradient fade + click area */}
